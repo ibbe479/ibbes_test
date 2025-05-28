3a78cbc9f242075186f37c9bbe30b9f5f4ad6784
ibbe479 <ibrahim.halawani04@gmail.com>
2025-05-29 00:01:27 +0200

    d

--- wiki.py
+++ wiki.py
@@ -1,46 +1,17 @@
-from bottle import Bottle, run, template, request, redirect, static_file
-import psycopg2
+from flask import Flask, render_template, request
 
-def connect_to_db():
-    try:
-        conn = psycopg2.connect(
-            host="pgserver.mau.se",
-            database="test_ibbe",
-            user="ao7391",
-            password="8hk5hh1b",
-            port="5432"
-        )
-        return conn
-    except psycopg2.Error as e:
-        print("Kunde inte ansluta till databasen:", e)
-        return None
+app = Flask(__name__)
 
-app = Bottle()
-
-@app.route("/")
+@app.route('/')
 def index():
-    return template("index", error="")
+    return render_template('index.html')
 
-@app.route("/login", method="POST")
-def login():
-    username = request.forms.get("username")
-    password = request.forms.get("password")
+@app.route('/submit', methods=['POST'])
+def submit():
+    namn = request.form['namn']
+    lösenord = request.form['lösenord']
+    # Här skulle man normalt spara till en databas (SQL)
+    return f"<h2>Hej {namn}!</h2><p>Ditt lösenord är: {lösenord}</p>"
 
-    conn = connect_to_db()
-    if conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM log_in WHERE user_name=%s AND lösenord=%s", (username, password))
-        user = cursor.fetchone()
-        conn.close()
-        if user:
-            return f"<h2>Välkommen, {username}!</h2><a href='/'>Tillbaka</a>"
-        else:
-            return template("index", error="Fel användarnamn eller lösenord")
-    else:
-        return template("index", error="Databasfel")
-
-@app.route("/static/<filename>")
-def static_files(filename):
-    return static_file(filename, root="./static")
-
-run(app, host="localhost", port=8080, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
